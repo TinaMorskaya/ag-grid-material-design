cb4dd636c40e39f81f61fface789f14e027365b8
Daria Astapenko <ms.daria.astapenko@gmail.com>
2020-11-01 11:35:50 +0300

    add column checkbox

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
-
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import "./App.css";
 
 import data from './data/bank-accounts.json';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+    },
+  });
 
 const App = () => {
     const [gridApi, setGridApi] = useState(null);
     const [gridColumnApi, setGridColumnApi] = useState(null);
 
     const [rowData, setRowData] = useState(data);
+
+    const classes = useStyles();
 
     function onGridReady(params) {
         setGridApi(params.api);
@@ -27,19 +41,37 @@ const App = () => {
     }
 
     return (
-        <div className="ag-theme-alpine" style={ { height: 400, width: 1200 } }>
-            <button onClick={onButtonClick}>Get selected rows</button>
-            <AgGridReact
-                rowData={rowData}
-                rowSelection="multiple"
-                onGridReady={onGridReady}>
-                <AgGridColumn field="account number" sortable={true} filter={true} checkboxSelection={true}></AgGridColumn>
-                <AgGridColumn field="account type" sortable={true} filter={true} checkboxSelection={true}></AgGridColumn>
-                <AgGridColumn field="currency" sortable={true} filter={true} checkboxSelection={true}></AgGridColumn>
-                <AgGridColumn field="balance" sortable={true} filter={true} checkboxSelection={true}></AgGridColumn>
-                <AgGridColumn field="bank name" sortable={true} filter={true} checkboxSelection={true}></AgGridColumn>
-                <AgGridColumn field="bank country" sortable={true} filter={true} checkboxSelection={true}></AgGridColumn>
-            </AgGridReact>
+        //<div className={classes.root} >
+        <div style={{ width: '100%', height: '100%' }}>
+            <div className="example-wrapper">
+                <div className="ag-theme-material" id="myGrid" style={{height: '100%', width: '100%'}}>
+                    <button onClick={onButtonClick}>Get selected rows</button>
+                    <AgGridReact
+                        defaultColDef={{
+                            flex: 1,
+                            minWidth: 100,
+                            resizable: true,
+                        }}
+                        rowData={rowData}
+                        rowSelection="multiple"
+                        onGridReady={onGridReady}
+                        suppressRowClickSelection={true}>
+                        <AgGridColumn 
+                            field="account number" 
+                            sortable={true} 
+                            filter={true} 
+                            headerCheckboxSelection={true}
+                            headerCheckboxSelectionFilteredOnly={true}
+                            checkboxSelection={true}>
+                        </AgGridColumn>
+                        <AgGridColumn field="account type" sortable={true} filter={true}></AgGridColumn>
+                        <AgGridColumn field="currency" sortable={true} filter={true}></AgGridColumn>
+                        <AgGridColumn field="balance" sortable={true} filter={true}></AgGridColumn>
+                        <AgGridColumn field="bank name" sortable={true} filter={true}></AgGridColumn>
+                        <AgGridColumn field="bank country" sortable={true} filter={true}></AgGridColumn>
+                    </AgGridReact>
+                </div>
+            </div>
         </div>
     );
 };
