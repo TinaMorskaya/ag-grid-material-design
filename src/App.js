3d7d134d7b39861366be1af7a39cf950b94c3c9c
Daria Astapenko <ms.daria.astapenko@gmail.com>
2020-11-02 20:31:19 +0300

    finish grid design

--- src/App.js
+++ src/App.js
@@ -6,16 +6,40 @@ import data from './data/bank-accounts.json';
 
 import { makeStyles } from '@material-ui/core/styles';
 
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
 const useStyles = makeStyles({
-    root: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      border: 0,
-      borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: 'white',
-      height: 48,
-      padding: '0 30px',
+
+    mainGrid: {
+        height: 'auto',
+        border: '1px transparent',
+        borderRadius: '10px',
+        overflow: 'hidden',
+        boxShadow: '0 13px 27px -15px rgba(50,50,93,.25), 0 8px 16px -8px rgba(0,0,0,.3), 0 -6px 16px -6px rgba(0,0,0,.025)'
     },
+
+    headerColumn: {
+        fontWeight: 'bold',
+        fontSize: '1.6em',
+        color: '#34515e',
+    },
+
+    cell: {
+        color: '#1c313a',
+    },
+
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+    },
+      
+    myGrid: {
+        flex: '1 1 0px',
+        width: '100%'
+    },
+
   });
 
 const App = () => {
@@ -41,39 +65,75 @@ const App = () => {
     }
 
     return (
-        //<div className={classes.root} >
         <div style={{ width: '100%', height: '100%' }}>
-            <div className="example-wrapper">
-                <div className="ag-theme-material" id="myGrid" style={{height: '100%', width: '100%'}}>
+            <div className={classes.wrapper}>
+                <div className={`ag-theme-material ${classes.myGrid}`} style={{height: '100%', width: '100%'}}>
                     <button onClick={onButtonClick}>Get selected rows</button>
                     <AgGridReact
                         defaultColDef={{
                             flex: 1,
                             minWidth: 100,
                             resizable: true,
+                            headerCheckboxSelection: isFirstColumn,
+                            checkboxSelection: isFirstColumn,
+                            cellClass: classes.cell,
+                            editable: true
                         }}
+                        enterMovesDownAfterEdit={true}
                         rowData={rowData}
                         rowSelection="multiple"
                         onGridReady={onGridReady}
-                        suppressRowClickSelection={true}>
-                        <AgGridColumn 
-                            field="account number" 
-                            sortable={true} 
-                            filter={true} 
-                            headerCheckboxSelection={true}
-                            headerCheckboxSelectionFilteredOnly={true}
-                            checkboxSelection={true}>
-                        </AgGridColumn>
-                        <AgGridColumn field="account type" sortable={true} filter={true}></AgGridColumn>
-                        <AgGridColumn field="currency" sortable={true} filter={true}></AgGridColumn>
-                        <AgGridColumn field="balance" sortable={true} filter={true}></AgGridColumn>
-                        <AgGridColumn field="bank name" sortable={true} filter={true}></AgGridColumn>
-                        <AgGridColumn field="bank country" sortable={true} filter={true}></AgGridColumn>
+                        suppressRowClickSelection={true}
+                        className={classes.mainGrid}
+                        domLayout='autoHeight'>
+                        <AgGridColumn field="account number" sortable={true} filter={true} headerClass={classes.headerColumn} minWidth={260} valueParser={newAccountNum}></AgGridColumn>
+                        <AgGridColumn field="account type" sortable={true} filter={true} headerClass={classes.headerColumn} cellEditor="agSelectCellEditor" cellEditorParams={{
+                            values: ['Checking', 'Savings']
+                            }}></AgGridColumn>
+                        <AgGridColumn field="currency" sortable={true} filter={true} headerClass={classes.headerColumn} editable={false}></AgGridColumn>
+                        <AgGridColumn field="balance" sortable={true} filter={true} headerClass={classes.headerColumn} type='rightAligned'  valueParser={newBalance} valueFormatter={curBalance}></AgGridColumn>
+                        <AgGridColumn field="bank name" sortable={true} filter={true} headerClass={classes.headerColumn}></AgGridColumn>
+                        <AgGridColumn field="bank country" sortable={true} filter={true} headerClass={classes.headerColumn}></AgGridColumn>
                     </AgGridReact>
                 </div>
             </div>
         </div>
     );
 };
+
+
+function isFirstColumn(params) {
+    let displayedColumns = params.columnApi.getAllDisplayedColumns();
+    let thisIsFirstColumn = displayedColumns[0] === params.column;
+    return thisIsFirstColumn;
+}
+
+function newAccountNum (params) {
+    let valueAsNumber;
+    const num = Number(params.newValue);
+    valueAsNumber = !num || !(num > 9999999) || !Number.isInteger(num)
+        ? 'Write a nine-digit number'
+        : num;
+    return valueAsNumber;
+
+}
+
+function curBalance(params) {
+    return currencyFormatter(params.value)
+}
+
+function currencyFormatter (value) {
+    let valueAsNumber;
+    const num = Number(value);
+    valueAsNumber = !num
+        ? 'Write a number'
+        : num.toLocaleString('en-EN', { style: 'currency', currency: 'USD' })
+    return valueAsNumber;
+}
+
+function newBalance(params) {
+    return currencyFormatter(params.newValue);
+}
+
 
 export default App;
